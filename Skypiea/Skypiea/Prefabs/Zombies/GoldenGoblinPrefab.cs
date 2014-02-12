8fe79259ff4dbc82669195af8f77a1e9b01c0e40
JaakkoLipsanen <jaakko.lipsanen@outlook.com>
2014-02-12 23:06:25 +0200

    Adjusted GG life

--- Skypiea/Skypiea/Prefabs/Zombies/GoldenGoblinPrefab.cs
+++ Skypiea/Skypiea/Prefabs/Zombies/GoldenGoblinPrefab.cs
@@ -15,7 +15,7 @@ namespace Skypiea.Prefabs.Zombies
             entity.Transform.Position = parameters.Get<Vector2>(0);
             entity.AddFromPool<CGoldenGoblinAI>().Initialize(entity.Transform.Position);
             entity.AddFromPool<CZombieInfo>().Initialize(ZombieType.GoldenGoblin, GoldenGoblinPrefab.Size);
-            entity.AddFromPool<CHealth>().Initialize(145);
+            entity.AddFromPool<CHealth>().Initialize(125);
             entity.AddFromPool<CParticleEmitter2D>().Initialize(ParticleEffectID.GoldenGoblinPath, new BurstTriggerController(0.025f, entity.Transform));
 
             // okay.. awful... entities can have only one component of a single type so I must create a child entity...
