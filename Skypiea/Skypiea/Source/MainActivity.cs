1e8b9829869f7da0ea4246118c7e6839d4de73a3
Jaakko Lipsanen <jaakko.lipsanen@outlook.com>
2017-07-21 18:15:28 +0300

    Working version

---
+++ Skypiea/Skypiea/Source/MainActivity.cs
@@ -0,0 +1,27 @@
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Views;
+
+namespace Skypiea
+{
+    [Activity(Label = "Skypiea"
+        , MainLauncher = true
+        , Icon = "@drawable/icon"
+        , Theme = "@style/Theme.Splash"
+        , AlwaysRetainTaskState = true
+        , LaunchMode = Android.Content.PM.LaunchMode.SingleInstance
+        , ScreenOrientation = ScreenOrientation.FullUser
+        , ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize)]
+    public class Activity1 : Microsoft.Xna.Framework.AndroidGameActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            var g = new SkypieaGame();
+            SetContentView((Android.Views.View)g.Services.GetService(typeof(Android.Views.View)));
+            g.Run();
+        }
+    }
+}
+
